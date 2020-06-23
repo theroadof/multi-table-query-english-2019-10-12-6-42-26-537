5f182c3fbdf2f60f9fa853e24d22884b16fb8257
theroadof <1294911641@qq.com>
2020-06-23 15:57:09 +0800

    commit

--- select.sql
+++ select.sql
@@ -5,9 +5,9 @@ select * from course where id = 1;
 select * from course where id = 1 or id =2;
 
 # 3.Query the student number and student name and average score of students whose average score is 60 or higher.
-select student.id,student.name,avg_score 
-from student ,(select studentId, avg(score) as avg_score from student_course group by studentId) as tmp
-where student.id = tmp.studentId and tmp.avg_score>=60.0;
+select student.id,student.name,avg(score) as avg_score
+from student left join student_course on student.id = student_course.studentId
+group by id,name having avg_score >=60;
 
 # 4.Query the SQL statement of student information that does not have grades in the student_course table
 select * from student left join student_course on student.id = student_course.studentId
@@ -27,9 +27,10 @@ where student_course.score<60
 order by student_course.score desc;
 
 # 8.Query the average grade of each course. The results are ranked in descending order of average grade. When the average grades are the same, they are sorted in ascending order by course number.
-select id,name,avg_score 
-from course,(select courseId,avg(score) as avg_score from student_course group by courseId order by avg_score desc) as tmp
-where course.id = tmp.courseId;
+select courseId,name,avg(score) as avg_score
+from student_course left join course on student_course.courseId = course.id
+group by courseId,name
+order by avg_score desc,courseId asc;
 
 # 9.Query the name and score of a student whose course name is "Math" and whose score is less than 60
 select student.name,score
